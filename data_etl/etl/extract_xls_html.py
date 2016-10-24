f1fa65e835a1e647baa0b39b345fe7e927116388
Tom Marks <thomas.o.marks@gmail.com>
2016-10-23 20:30:49 -0400

    Adds ability to extract data from delaware business license files

--- data_etl/etl/extract_xls_html.py
+++ data_etl/etl/extract_xls_html.py
@@ -1,25 +1,35 @@
-from collections import namedtuple
-
 from bs4 import BeautifulSoup
 
-Data = namedtuple('Data', ['headers'])
+NON_BREAKING_WHITESPACE = u'\xa0'
 
 
 class Data(object):
 
-    def __init__(self, html_str):
+    def __init__(self, html_str, headers=None, skip_rows=0):
         self.soup = BeautifulSoup(html_str, 'html.parser')
-        self.rows = iter(self.soup.html.body.table.find_all('tr'))
-        self.headers = [
-            unicode(header.string) for header in
-            next(self.rows).find_all('td')
-        ]
+        self.rows = iter(self.soup.table.find_all('tr'))
+        if headers is None:
+            self.headers = [
+                unicode(header.string) for header in
+                next(self.rows).find_all('td')
+            ]
+            if (skip_rows - 1) > 0:
+                for _ in range(skip_rows - 1):
+                    next(self.rows)
+        else:
+            self.headers = headers
+            if (skip_rows) > 0:
+                for _ in range(skip_rows):
+                    next(self.rows)
 
     def __iter__(self):
         for row in self.rows:
-            data = [unicode(col.string) for col in row.find_all('td')]
+            data = [
+                unicode(col.string).replace(NON_BREAKING_WHITESPACE, ' ')
+                for col in row.find_all('td')
+            ]
             yield dict(zip(self.headers, data))
 
 
-def extract_data_from_html(html_str):
-    return Data(html_str)
+def extract_data_from_html(html_str, headers=None, skip_rows=0):
+    return Data(html_str, headers=headers, skip_rows=skip_rows)
--- data_etl/tests/unit/test_extract_xls_html.py
+++ data_etl/tests/unit/test_extract_xls_html.py
@@ -1,7 +1,7 @@
 import unittest
 
 from etl import extract_xls_html as etl
-from fixtures import TEST_CONTRIBUTIONS_LIST
+from fixtures import TEST_CONTRIBUTIONS_LIST, TEST_BUSINESS_LICENSE_LIST
 
 
 class TestExtractHtmlData(unittest.TestCase):
@@ -43,6 +43,61 @@ class TestExtractHtmlData(unittest.TestCase):
                 }
             ]
         )
+
+    def test_extracts_business_list_headers(self):
+        headers = [
+            'Business name',
+            'Trade name',
+            'Category',
+            'From',
+            'To',
+         ]
+
+        data = etl.extract_data_from_html(
+            TEST_BUSINESS_LICENSE_LIST, headers=headers)
+
+        self.assertEqual(data.headers, headers)
+
+    def test_extracts_business_list_data_to_py_objs(self):
+
+        headers = [
+            'Business name',
+            'Trade name',
+            'Category',
+            'Current License Valid From',
+            'Current License Valid To',
+            'Address 1',
+            'Address 2',
+            'City',
+            'State',
+            'Zip',
+            'Country',
+            'License number'
+        ]
+
+        expected = [
+            {
+                'Business name': u'000 CHOICE MANAGEMENT INC',
+                'Trade name': u' ',
+                'Category': u'PROFESSIONAL AND/OR PRSL SRVCS-UNCLASSIFIED',
+                'Current License Valid From': u'1/1/05',
+                'Current License Valid To': u'12/31/07',
+                'Address 1': u' ',
+                'Address 2': u'7904 COASTAL HWY #5',
+                'City': u'OCEAN CITY',
+                'State': u'MD',
+                'Zip': u'21842-6707',
+                'Country': u' ',
+                'License number': u'1999202399',
+            },
+        ]
+
+        data = etl.extract_data_from_html(
+            TEST_BUSINESS_LICENSE_LIST, headers=headers, skip_rows=2)
+
+        self.maxDiff = None
+        for got, expect in zip(list(data), expected):
+            self.assertEqual(got, expect)
 
 
 if __name__ == '__main__':
